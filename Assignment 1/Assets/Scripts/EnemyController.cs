6ec4b85ea726759b3a9d920b1dece1fa4ae14a4e
kazuma-sato <kazuma.sato@georgebrown.ca>
2016-09-29 00:11:28 -0400

    Push
    Added Player, Enemy and Bullet sprites.
    Added EnemyController and PlayerController classes.
    Created file structure for assets.

---
+++ Assignment 1/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// COMP3064 Assignment 1
+// Due Date: Sunday, October 30, 2016
+// Instructor: Przemyslaw Pawluk
+// Kazuma Sato 100 948 212
+
+public class EnemyController : MonoBehaviour {
+	
+	[SerializeField]
+	private Vector2 speed = Vector2.zero;
+
+	private Transform _transform;
+	private Vector2 _currentPosition;
+	private float _xBounds;
+
+	//direction: positive when moving up, negative when moving down
+	private int direction = 1;
+
+	void Start(){
+		_transform = gameObject.GetComponent<Transform>();
+		_currentPosition = _transform.position;
+		_xBounds = Camera.main.orthographicSize * Camera.main.aspect;
+		Reset ();
+	}
+
+	void Update(){
+		_currentPosition = _transform.position;
+		Vector2 currSpeed = new Vector2(speed.x, speed.y * direction);
+		_currentPosition += currSpeed;
+		_transform.position = _currentPosition;
+
+		Debug.Log (_xBounds + " " + _currentPosition.y.ToString()); 
+		if(_currentPosition.x <= _xBounds) {
+			Reset();
+		}
+	}
+
+	public void Reset(){
+		Debug.Log("Oh shit! Im resetting!!!");
+		direction *= -1;
+		_currentPosition = new Vector2(-_xBounds, direction * Camera.main.orthographicSize);
+		Debug.Log (_currentPosition.ToString ());
+		_transform.position = _currentPosition;
+	}
+}
---
+++ Assignment 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// COMP3064 Assignment 1
+// Due Date: Sunday, October 30, 2016
+// Instructor: Przemyslaw Pawluk
+// Kazuma Sato 100 948 212
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed;
+
+	private Transform _transform;
+	private Vector2 _currentPosition;
+	private float _playerInput;
+	private float _yBounds;
+
+	void Start () {
+		_transform = gameObject.GetComponent<Transform>();
+		_currentPosition = _transform.position;
+		_yBounds = Camera.main.orthographicSize;
+	}
+
+	void Update () {
+		_playerInput = Input.GetAxis("Vertical");
+		_currentPosition = _transform.position;
+		//move up
+		if (_playerInput > 0) {
+			_currentPosition += new Vector2 (0, speed);
+		}
+		//move down
+		if (_playerInput < 0) {
+			_currentPosition -= new Vector2 (0, speed);
+		}
+		//fix bounds
+		checkBounds ();
+		_transform.position = _currentPosition;
+	}
+	private void checkBounds(){
+
+		if (_currentPosition.y < -1 * _yBounds) {
+			_currentPosition.y = -1 * _yBounds;
+		}
+		if (_currentPosition.y > _yBounds) {
+			_currentPosition.y = _yBounds;
+		}
+	}
+}
